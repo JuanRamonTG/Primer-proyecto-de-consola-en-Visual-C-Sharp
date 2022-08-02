2e3f0dd271adeda12434787d09c782062bc4ec6b
Juan Ramón Torres Guzmán <USIS054021@ugb.edu.sv>
2022-08-01 23:07:44 -0600

    cambioi

--- Program.cs
+++ Program.cs
@@ -6,30 +6,37 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            // Declare variables and then initialize to zero.
+            
             int num1 = 0; int num2 = 0;
 
-            // Display title as the C# console calculator app.
+            
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Consola De Calculadora C#\r");
             Console.WriteLine("------------------------\n");
 
-            // Ask the user to type the first number.
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Escriba un número y luego presione Enter");
             num1 = Convert.ToInt32(Console.ReadLine());
 
-            // Ask the user to type the second number.
-            Console.WriteLine("");
+
+            
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Escriba un número y luego presione Enter");
             num2 = Convert.ToInt32(Console.ReadLine());
 
-            // Ask the user to choose an option.
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Elija una opción de la siguiente lista:");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("\ta - Add");
             Console.WriteLine("\ts - Subtract");
             Console.WriteLine("\tm - Multiply");
             Console.WriteLine("\td - Divide");
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.Write("Tu opcion? ");
 
-            // Use a switch statement to do the math.
+            
             switch (Console.ReadLine())
             {
                 case "a":
@@ -45,7 +52,7 @@ namespace Calculadora
                     Console.WriteLine($"Tu resultado: {num1} / {num2} = " + (num1 / num2));
                     break;
             }
-            // Wait for the user to respond before closing.
+            
             Console.Write("Presione cualquier tecla para cerrar la aplicación de la consola Calculadora...");
             Console.ReadKey();
         }
